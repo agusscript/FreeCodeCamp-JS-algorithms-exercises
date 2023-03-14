3b44cacc32d4086f0b5077d5429285d32df3a05c
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-03-14 09:15:28 -0300

    Algorithm solved

---
+++ Intermediate-Algorithm-Scripting/04-Wherefore art thou/solution.js
@@ -0,0 +1,23 @@
+function whatIsInAName(collection, source) {
+  let sourceKeys = Object.keys(source);
+
+  let filteredObj = collection.filter((obj) => {
+    for (let i = 0; i < sourceKeys.length; i++) {
+      if (obj[sourceKeys[i]] !== source[sourceKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  return filteredObj;
+}
+
+whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" },
+  ],
+  { last: "Capulet" }
+);
